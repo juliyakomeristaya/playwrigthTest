220096655326a88467547ddd03011f4d4fdabd25
Juliya Komeristaya <juliyakomeristaya@gmail.com>
2025-10-22 20:30:40 +0300

    stepDecoretor

--- pom/pages/HomePage.ts
+++ pom/pages/HomePage.ts
@@ -1,15 +1,18 @@
 import { Locator } from "@playwright/test";
 import BasePage from "../BasePage";
+import { step } from "../../utils/stepDecoretor";
 
 export default class HomePage extends BasePage {
   private readonly signInButton: Locator = this.page.locator(
     '//button[contains(@class, "header_signin")]',
   );
 
+  @step("Navigate to home page")
   async navigate(): Promise<void> {
     await this.page.goto("/");
   }
 
+  @step("Open Sign In form")
   async openSignInForm(): Promise<void> {
     await this.signInButton.click();
   }
@@ -18,6 +21,7 @@ export default class HomePage extends BasePage {
     '//button[contains(@class, "hero-descriptor_btn btn btn-primary")]',
   );
 
+  @step("Open Sign Up form")
   async openSignUPForm(): Promise<void> {
     await this.signUpButton.click();
   }
